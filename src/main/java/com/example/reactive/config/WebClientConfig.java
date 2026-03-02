58e2730991926cead879dfcfddd40ed87ed94114
RAJUNAIDUJAVA <rajunaidusingampalli48@gmail.com>
2026-03-02 20:50:20 +0530

    Read Timeout implemented for rest api

--- src/main/java/com/example/reactive/config/WebClientConfig.java
+++ src/main/java/com/example/reactive/config/WebClientConfig.java
@@ -2,7 +2,12 @@ package com.example.reactive.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
@@ -11,7 +16,14 @@ public class WebClientConfig {
     public WebClient webClient(){
 
         return WebClient.builder().baseUrl("http://localhost:8081")
+                .clientConnector(new ReactorClientHttpConnector(callHttpClient())) // here we given HttpClient for read timeout
                 .build();
 
+    }
+
+    public HttpClient callHttpClient(){
+        HttpClient httpClient = HttpClient.create()
+                .responseTimeout(Duration.ofSeconds(20)); // configured 20 secs read timeout
+        return httpClient;
     }
 }
