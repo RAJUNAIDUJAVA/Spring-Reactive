7257732dde2b08a5f58453574c9e118d7a047380
RAJUNAIDUJAVA <rajunaidusingampalli48@gmail.com>
2026-03-03 15:45:32 +0530

    Default headers added for the downstream service

--- src/main/java/com/example/reactive/config/WebClientConfig.java
+++ src/main/java/com/example/reactive/config/WebClientConfig.java
@@ -2,6 +2,7 @@ package com.example.reactive.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
@@ -11,6 +12,7 @@ import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Configuration
@@ -28,9 +30,20 @@ public class WebClientConfig {
 
         return WebClient.builder()
                 .uriBuilderFactory(factory)
+                //.defaultHeader("xnmUid","x-nm-uid") // sending single header
+                .defaultHeaders(httpHeaders -> {
+                    httpHeaders.set("xnmUid","x-nm-Uid"); // added multiple header
+                    httpHeaders.set("xnmRequestId","12345"); // added multiple default headers
+                })
                 .clientConnector(new ReactorClientHttpConnector(callHttpClient())) // here we given HttpClient for read timeout
                 .build();
 
+    }
+
+    public HttpHeaders httpHeaders(){
+        HttpHeaders headers= new HttpHeaders();
+        headers.put("xnmUid", List.of("x-nm-Uid"));
+        return headers;
     }
 
     public HttpClient callHttpClient(){
